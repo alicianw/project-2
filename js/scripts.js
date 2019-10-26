2376081d3322d8cd77a4bf8b212b00799e7dd49d
alicia <alicianyambu@gmail.com>
2019-10-26 14:07:06 +0300

    added border and modified js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,5 @@
-function myFrm() {
-    var day = parseInt(document.getElementById("day").value);
-    var month = parseInt(document.getElementById("month").value);
-
-    var year = document.getElementById("year").value;
-   
-    var cc = parseInt(year.slice(0, 2));
-    var yy = parseInt(year.slice(2, 4));
-
-    var gender = document.getElementById("gender").value;
+function myFrm(){
+  var m = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var f = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   
-    var fml =( ( (cc/4) - 2 * (cc - 1)) + ((5*yy/4) ) + ((26*(month+1)/day)) + day )% 7;
-    var bdate = fml.toFixed(0);
-    
-     
-     var mname = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-      var fname = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-     var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    if (gender == "male"){
-      return  document.getElementById("result").innerHTML = mname[bdate-1];
-      }
-  
-      if (gender == "female"){
-        return  document.getElementById("result").innerHTML = fname[bdate-1];
-      }
-
-  }
-  
+}
