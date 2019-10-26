0ada5492e08d7d2a5cb57e521b4f094ff02dde3d
alicia <alicianyambu@gmail.com>
2019-10-26 15:29:44 +0300

    removed container for label

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
-function myFrm(){
+function myFrm() {
   var m = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var f = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-  var g = document.getElementsById("gender").value;
-  var d = document.getElementsById("day").value;
-  var mnth = document.getElementsById("month").value;
-  var y = document.getElementsById("year").value;
+  
+  var d = document.getElementsByName("day").value;
+  var mnth = document.getElementsByName("month").value;
+  var y = document.getElementsByName("year").value;
+  var g = document.getElementsByName("gen").value;
 
   var date = new Date(`${y}-${mnth}-${d}`);
   var day = date.getDay();
+  console.log(g);
   if (g=="male"){
-    return document.getElementsById("val").innerHTML=m[day] +" "+days[day];
+    return document.getElementsByName("val").innerHTML = m[day] +" "+days[day];
   }
   if (g=="female"){
-    return document.getElementsById("val").innerHTML=f[day] +" "+days[day];
+    return document.getElementsByName("val").innerHTML = f[day] +" "+days[day];
   }
 }
