b8baf5080d8ad74f7f159bd2667a81f21bfaac6d
alicia <alicianyambu@gmail.com>
2019-10-26 11:29:51 +0300

    re-started HTML code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 function myFrm() {
     var day = parseInt(document.getElementById("day").value);
     var month = parseInt(document.getElementById("month").value);
-   var year = document.getElementById("year").value;
+
+    var year = document.getElementById("year").value;
+   
     var cc = parseInt(year.slice(0, 2));
     var yy = parseInt(year.slice(2, 4));
+
     var gender = document.getElementById("gender").value;
   
     var fml =( ( (cc/4) - 2 * (cc - 1)) + ((5*yy/4) ) + ((26*(month+1)/day)) + day )% 7;
@@ -12,6 +15,7 @@ function myFrm() {
      
      var mname = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
       var fname = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+     var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
     if (gender == "male"){
       return  document.getElementById("result").innerHTML = mname[bdate-1];
       }
