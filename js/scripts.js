a10b770f353b3eb373ea319a4c3f7b0d83275733
alicia <alicianyambu@gmail.com>
2019-10-26 14:19:03 +0300

    modified js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@ function myFrm(){
   var m = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var f = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  var g = document.getElementsById("gender").value;
+  var d = document.getElementsById("day").value;
+  var mnth = document.getElementsById("month").value;
+  var y = document.getElementsById("year").value;
 }
