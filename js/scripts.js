c139d3adac638a6d744aee2d96044101194a6a48
alicia <alicianyambu@gmail.com>
2019-10-26 14:10:40 +0300

    added weekday array in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function myFrm(){
   var m = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var f = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 }
